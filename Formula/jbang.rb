bcf2121dc21fa22ea5feb3c7ef10a1e3d51e816c
Max Rydahl Andersen <max@xam.dk>
2019-12-26 09:13:06 +0100

    jbang v0.0.4.2

--- Formula/jbang.rb
+++ Formula/jbang.rb
@@ -1,14 +1,12 @@
-class Jbang < Formula
-    desc "jbang"
-    homepage "https://github.com/maxandersen/jbang"
-    url "https://github.com/maxandersen/jbang/releases/download/v0.0.4/jbang-0.0.4.zip"
-    sha256 "a7e809aaab00668c1f668db85b25dc46082e60dc30102e62c8843d707e79517f"
-  
-    #depends_on cask:"java"
-  
-    def install
-      libexec.install Dir["*"]
-      inreplace "#{libexec}/bin/jbang", /^jarPath=.*/, "jarPath=#{libexec}/bin/jbang.jar"
-      bin.install_symlink "#{libexec}/bin/jbang"
-    end
+class JBang < Formula
+  desc "jbang"
+  homepage "https://github.com/maxandersen/jbang"
+  url "https://github.com/maxandersen/jbang/releases/download/v0.0.4.2/jbang-0.0.4.2.zip"
+  sha256 "3de5df7b0e1760bb5a56bfc7a8068d6d930cd3f80a20205d9c6efb969c07ba29"
+
+  def install
+    libexec.install Dir["*"]
+    inreplace "#{libexec}/bin/kscript", /^jarPath=.*/, "jarPath=#{libexec}/bin/jbang.jar"
+    bin.install_symlink "#{libexec}/bin/jbang"
   end
+end
