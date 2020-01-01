420cc786d2103f3138712fa48410ff3e523edabf
Max Rydahl Andersen <max@xam.dk>
2020-01-01 02:25:31 +0000

    jbang v0.1.2.3

--- Formula/jbang.rb
+++ Formula/jbang.rb
@@ -1,8 +1,8 @@
 class Jbang < Formula
     desc "jbang"
     homepage "https://github.com/maxandersen/jbang"
-    url "https://github.com/maxandersen/jbang/releases/download/v0.1.2.2/jbang-0.1.2.2.zip"
-    sha256 "ad9ca1ac3ba5a50c5825cd2bb34b542c1f9fa6f127a93994c1398a3b86241cd8"
+    url "https://github.com/maxandersen/jbang/releases/download/v0.1.2.3/jbang-0.1.2.3.zip"
+    sha256 "ff32ccb7af2f635462c837e4be46facc581798493680833deb769f2aece39c87"
   
     #depends_on cask:"java"
   
