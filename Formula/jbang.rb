9cb6968fd4377e699f631b288d97ef84b31bab10
Max Rydahl Andersen <max@xam.dk>
2020-01-29 15:06:01 +0000

    jbang v0.13.0

--- Formula/jbang.rb
+++ Formula/jbang.rb
@@ -1,14 +1,24 @@
 class Jbang < Formula
-    desc "jbang"
+    desc "Unleash the power of Java for shell scripting"
     homepage "https://github.com/maxandersen/jbang"
-    url "https://github.com/maxandersen/jbang/releases/download/v0.12.2/jbang-0.12.2.zip"
-    sha256 "33a493fa568d1995d691880e898f2f61ade15885450102ee70626e8320e792d7"
-  
+    url "https://github.com/maxandersen/jbang/releases/download/v0.13.0/jbang-0.13.0.zip"
+    sha256 "d1c01ddbfd1c3b113f44f4721bdf79147b8207340f0633fab83b61d898459dc7"
+
+    bottle :unneeded
+
+    keg_only :versioned_formula
+
     #depends_on cask:"java"
-  
+    depends_on :java => "1.8+"
+
     def install
       libexec.install Dir["*"]
       inreplace "#{libexec}/bin/jbang", /^jarPath=.*/, "jarPath=#{libexec}/bin/jbang.jar"
       bin.install_symlink "#{libexec}/bin/jbang"
+    end
+
+    test do
+        system "#{bin}/jbang", "--init=cli", "hello.java"
+        system "#{bin}/jbang", "hello.java", "Homebrew!"
     end
   end
