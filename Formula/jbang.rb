7c5d8abae91711f1895c0f59638a77209338cdda
Max Rydahl Andersen <max@xam.dk>
2020-09-24 23:14:16 +0000

    jbang v0.48.0

--- Formula/jbang.rb
+++ Formula/jbang.rb
@@ -1,8 +1,8 @@
 class Jbang < Formula
     desc "Unleash the power of Java for shell scripting"
     homepage "https://jbang.dev"
-    url "https://github.com/jbangdev/jbang/releases/download/v0.47.1/jbang-0.47.1.zip"
-    sha256 "1d45cdb398dc4ab37d655be1f3a8e6c86a0d60cd483cae9c766643b64b34f6a3"
+    url "https://github.com/jbangdev/jbang/releases/download/v0.48.0/jbang-0.48.0.zip"
+    sha256 "ea6e37a995a37fe3788c96788c156a90ff5279f8efba35d0e3ea8b1089f22f1e"
 
     bottle :unneeded
 
