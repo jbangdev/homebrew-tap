e922c453f5388d5bca1bee9739541692e5f45437
Max Rydahl Andersen <max@xam.dk>
2021-02-05 23:34:07 +0000

    jbang v0.65.1

--- Formula/jbang.rb
+++ Formula/jbang.rb
@@ -1,8 +1,8 @@
 class Jbang < Formula
     desc "Unleash the power of Java"
     homepage "https://jbang.dev"
-    url "https://github.com/jbangdev/jbang/releases/download/v0.65.0/jbang-0.65.0.zip"
-    sha256 "aeec58b915817deee6b91c62b448a832d29077742d0973763398aba545bb4910"
+    url "https://github.com/jbangdev/jbang/releases/download/v0.65.1/jbang-0.65.1.zip"
+    sha256 "90a9598323ef2fe2780c7214ebf38f507837945ae1629ddfae59c2adf34b2791"
 
     bottle :unneeded
 
