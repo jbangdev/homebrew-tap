7e03118bae82a8236e690ce29bc2b3dfb8a4e584
jreleaserbot <jreleaser@kordamp.org>
2022-08-02 12:31:58 +0000

    jbang v0.96.1

--- Formula/jbang.rb
+++ Formula/jbang.rb
@@ -1,9 +1,9 @@
 class Jbang < Formula
   desc "Unleash the power of Java"
   homepage "https://jbang.dev"
-  version "0.96.0"
-  url "https://github.com/jbangdev/jbang/releases/download/v0.96.0/jbang-0.96.0.zip"
-  sha256 "783a702bc5e7dff042a9c05c0520c4fe40afcb0536cb6a4b809637f935070475"
+  version "0.96.1"
+  url "https://github.com/jbangdev/jbang/releases/download/v0.96.1/jbang-0.96.1.zip"
+  sha256 "ca5d17ad9632b6375ff5103301a0617d7db806cc82bfa366635626e192c9488f"
   license "MIT"
 
 
